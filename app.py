531eaa585963780d44704aece74b50616726fed0
Satish Fulwani <satish.fulwani63@gmail.com>
2025-08-19 13:31:04 +0530

    Addition of requirement.txt and upgrading packages

--- app.py
+++ app.py
@@ -2,17 +2,16 @@
 import pandas as pd
 import numpy as np
 import pandas_datareader.data as web
-from fbprophet import Prophet
+from prophet import Prophet
 import datetime
-from flask import Flask, render_template
-from flask import request, redirect
+from flask import Flask, render_template, request
 from pathlib import Path
 import os
-import os.path
 import csv
 from itertools import zip_longest
 
 app = Flask(__name__)
+
 
 @app.after_request
 def add_header(response):
@@ -23,7 +22,8 @@ def add_header(response):
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=0'
     return response
-    
+
+
 @app.route("/")
 def first_page():
     tmp = Path("static/prophet.png")
@@ -34,78 +34,85 @@ def first_page():
         os.remove(tmp_csv)
     return render_template("index.html")
 
-#function to get stock data
+# function to get stock data
+
+
 def yahoo_stocks(symbol, start, end):
     return web.DataReader(symbol, 'yahoo', start, end)
 
+
 def get_historical_stock_price(stock):
-    print ("Getting historical stock prices for stock ", stock)
-    #get 7 year stock data
+    print("Getting historical stock prices for stock ", stock)
+    # get 7 year stock data
     startDate = datetime.datetime(2017, 1, 4)
     endDate = datetime.datetime(2019, 9, 15)
     stockData = yahoo_stocks(stock, startDate, endDate)
     return stockData
 
+
 @app.route("/predict")
 def second_page():
     return render_template("predict.html")
 
-@app.route("/plot" , methods = ['POST', 'GET'] )
+
+@app.route("/plot", methods=['POST', 'GET'])
 def main():
     if request.method == 'POST':
         stock = request.form['companyname']
         df_whole = get_historical_stock_price(stock)
 
         df = df_whole.filter(['Close'])
-        
+
         df['ds'] = df.index
-        #log transform the ‘Close’ variable to convert non-stationary data to stationary.
+        # log transform the ‘Close’ variable to convert non-stationary data to stationary.
         df['y'] = np.log(df['Close'])
         original_end = df['Close'][-1]
-        
+
         model = Prophet()
         model.fit(df)
 
-        #num_days = int(input("Enter no of days to predict stock price for: "))
-        
+        # num_days = int(input("Enter no of days to predict stock price for: "))
+
         num_days = 10
         future = model.make_future_dataframe(periods=num_days)
         forecast = model.predict(future)
-        
-        print (forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
-        
-        #Prophet plots the observed values of our time series (the black dots), the forecasted values (blue line) and
-        #the uncertainty intervalsof our forecasts (the blue shaded regions).
-        
-        #forecast_plot = model.plot(forecast)
-        #forecast_plot.show()
-        
-        #make the vizualization a little better to understand
+
+        print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
+
+        # Prophet plots the observed values of our time series (the black dots), the forecasted values (blue line) and
+        # the uncertainty intervalsof our forecasts (the blue shaded regions).
+
+        # forecast_plot = model.plot(forecast)
+        # forecast_plot.show()
+
+        # make the vizualization a little better to understand
         df.set_index('ds', inplace=True)
         forecast.set_index('ds', inplace=True)
-        #date = df['ds'].tail(plot_num)
-        
-        viz_df = df.join(forecast[['yhat', 'yhat_lower','yhat_upper']], how = 'outer')
+        # date = df['ds'].tail(plot_num)
+
+        viz_df = df.join(
+            forecast[['yhat', 'yhat_lower', 'yhat_upper']], how='outer')
         viz_df['yhat_scaled'] = np.exp(viz_df['yhat'])
 
-        #close_data = viz_df.Close.tail(plot_num)
-        #forecasted_data = viz_df.yhat_scaled.tail(plot_num)
-        #date = future['ds'].tail(num_days+plot_num)
+        # close_data = viz_df.Close.tail(plot_num)
+        # forecasted_data = viz_df.yhat_scaled.tail(plot_num)
+        # date = future['ds'].tail(num_days+plot_num)
 
         close_data = viz_df.Close
         forecasted_data = viz_df.yhat_scaled
         date = future['ds']
-        #date = viz_df.index[-plot_num:-1]
+        # date = viz_df.index[-plot_num:-1]
         forecast_start = forecasted_data[-num_days]
 
         d = [date, close_data, forecasted_data]
-        export_data = zip_longest(*d, fillvalue = '')
+        export_data = zip_longest(*d, fillvalue='')
         with open('static/numbers.csv', 'w', encoding="ISO-8859-1", newline='') as myfile:
             wr = csv.writer(myfile)
             wr.writerow(("Date", "Actual", "Forecasted"))
             wr.writerows(export_data)
         myfile.close()
-        return render_template("plot.html", original = round(original_end,2), forecast = round(forecast_start,2), stock_tinker = stock.upper())
+        return render_template("plot.html", original=round(original_end, 2), forecast=round(forecast_start, 2), stock_tinker=stock.upper())
+
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
